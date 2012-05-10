b7724ac7043cb83fc418e51a6e98923dd56c7ac5
Sveinbjörn J. Tryggvason <sjt@sjt.is>
2012-05-10 17:47:48 +0000

    Removing some debug std::cout statements

--- PC2Importer/PC2Importer.cpp
+++ PC2Importer/PC2Importer.cpp
@@ -42,10 +42,12 @@ PC2Importer::PC2Importer(const std::string &name, Node *parent) : Node(name, par
 	specs.clear();
 	specs.push_back("Vec3Array");
 	setAttributeAllowedSpecializations(_points, specs);
+	specs.clear();
+	specs.push_back("Path");
+	setAttributeAllowedSpecializations(_filename, specs);
 }
 
 PC2Importer::~PC2Importer(){
-	delete header;
 }
 
 void PC2Importer::updateSlice(Attribute *attribute, unsigned int slice){
@@ -77,30 +79,18 @@ void PC2Importer::updateSlice(Attribute *attribute, unsigned int slice){
 	//std::cout << "with tellg: " << headerSize << " with calc: " << sizeof(int)*3 + sizeof(float)*2 + sizeof(char) * 12 << std::endl;
 	float currentFrame = _frame->value()->floatValueAt(0) - header->startFrame;
 	
-	std::cout << "BOOL: " << _keep->value()->boolValueAt(0) << std::endl;
+	
 	if( _keep->value()->boolValueAt(0) ) {
 		if (_frame->value()->floatValueAt(0) < header->startFrame) {
-			std::cout << "LESS THAN " << header->startFrame << std::endl;
 			currentFrame = header->startFrame;
 		} else if (_frame->value()->floatValueAt(0) >= header->numSamples) {
-			std::cout << "GREATER THAN " << header->numSamples << std::endl;
 			currentFrame = header->numSamples-1;
-			std::cout << currentFrame << std::endl;
 		}
-	}
-	std::cout << currentFrame << std::endl;
-		
-	
-	//float currentFrame = _frame->value()->floatValueAt(0) - header->startFrame;
-	//std::cout << "_frame: " << _frame->value()->floatValueAt(0) << std::endl;
-	//std::cout << "currentFrame " << currentFrame << std::endl;
+	}		
 	float frame_size = header->numPoints * sizeof(float) * 3;
-	//std::cout << "frame_size " << frame_size << std::endl;
 	
 	float file_offset = currentFrame * frame_size + (float)headerSize; 
-	std::cout << "before seek" << file.tellg() << std::endl;
 	file.seekg((int)file_offset, std::ios::beg);
-	std::cout << "after seek" << file.tellg() << std::endl;	
 	std::vector<Imath::V3f> points;
 	
 	float *a_point = new float[3];
@@ -110,7 +100,6 @@ void PC2Importer::updateSlice(Attribute *attribute, unsigned int slice){
 		for(int j=0; j<3; j++){
 			file.read((char *)&a_point[j], sizeof(float));
 		}
-		//std::cout << "complete point:" << Imath::V3f(a_point[0],a_point[1],a_point[2]) << std::endl;
 		points.push_back(Imath::V3f(a_point[0],a_point[1],a_point[2]));
 	}
 	
